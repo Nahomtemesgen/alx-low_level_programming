643c082981bc0cecabbcd1a13cea03c5cd16a67e
Nahomtemesgen <101422942+Nahomtemesgen@users.noreply.github.com>
2022-04-18 23:04:07 -0700

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print digit from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
