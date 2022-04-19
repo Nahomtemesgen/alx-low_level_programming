15060b657ac4fc2dc6dab098380c92341c3db004
Nahomtemesgen <101422942+Nahomtemesgen@users.noreply.github.com>
2022-04-18 23:38:14 -0700

    puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - puts function that print a string
+ * @str: parameter pointer
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+puts(str);
+}
