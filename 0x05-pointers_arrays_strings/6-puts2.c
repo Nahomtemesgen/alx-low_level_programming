23fab7ff38e7477d5d9f391051e14e2a006c0007
Nahomtemesgen <101422942+Nahomtemesgen@users.noreply.github.com>
2022-04-19 04:21:39 -0700

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,22 @@
  #include "holberton.h"
-
 /**
- * puts2 - Write a function that prints every other character of a string, 
- *         starting with the first character, followed by a new line.
+ *puts2 - prints very other character in string.
+ *@str: pointer to string.
  *
- * @s: This pointer point to a char or string
- *
- *
+ *Return: void.
  */
-
-void puts2(char *s)
+void puts2(char *str)
 {
-	int a;
+	int i;
 
-	for (a = 0; s[a] != '\0'; a++)
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (a % 2 == 0)
+		if (i % 2 == 0)
 		{
-			_putchar(s[a]);
+			_putchar(str[i]);
 		}
+		i++;
 	}
 	_putchar('\n');
 }
