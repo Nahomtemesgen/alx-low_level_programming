c2c09314cf8f55445cd5d8255bf20063e3f6d886
Nahomtemesgen <101422942+Nahomtemesgen@users.noreply.github.com>
2022-04-18 23:37:33 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - print the length of string
+ * @s: parameter of function
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
