7bc9fe737555527516304dd8467030de711a4f1c
Nahomtemesgen <101422942+Nahomtemesgen@users.noreply.github.com>
2022-04-29 01:10:24 -0700

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* _sqrt_recursion - function
+* @n: integer to get square root of
+*
+* Description: function to find sqroot of given integer
+* Return: 0, return -1 if no square root.
+*/
+int _sqrt_recursion(int n)
+{
+	int c = 0;
+
+	if (c == n)
+	{
+		return (0);
+	}
+	return (0);
+}
