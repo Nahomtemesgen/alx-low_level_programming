6c3c2f0f008b19f5a3b775752d980f1294248b5c
Nahomtemesgen <101422942+Nahomtemesgen@users.noreply.github.com>
2022-04-29 01:11:47 -0700

    Create 7-is_palindrome.c

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* is_palindrome - function
+* @s: string to check for palindrome
+*
+* Description: func to check if string is palindrome
+* Return: 1 if palindrome, 0 if not
+*/
+int is_palindrome(char *s)
+{
+	if (*s)
+	{
+		return (0);
+	}
+	return (0);
+}
