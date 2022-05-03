e990b1e18f3c719e2b1cf93fe1ab8e3fcffba3ea
Nahomtemesgen <101422942+Nahomtemesgen@users.noreply.github.com>
2022-05-03 13:38:01 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,52 +1,49 @@
-#include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "holberton.h"
 
 /**
- * str_concat - function that concatenates two strings.
- *              if NULL is passed, treat it as an empty string
- *              The function should return NULL on failure
- *
- * @s1: This is the output string
- * @s2: This is the input string
- *
- * Return: The returned pointer should point to a newly allocated space in
- *         memory which contains the contents of s1, followed by the contents 
- *         of s2, and null terminated
+ * *str_concat - concatenates two strings
+ * @s1: first string to concatenate
+ * @s2: second string to concatenate
+ * Return: pointer to the new string created (Success), or NULL (Error)
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	int a, b;
-	char *s;
+	char  *s3;
+	unsigned int i, j, len1, len2;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	for (a = 0; s1[a] != '\0'; a++)
-	{
-	}
-	for (b = 0; s2[b] != '\0'; b++)
-	{
-	}
+	i = 0;
+	j = 0;
+	len1 = 0;
+	len2 = 0;
 
-	s = (char *)  malloc(((a + b) + 1) * sizeof(char));
-
-	if (s == NULL)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
 		return (NULL);
-	for (a = 0; s1[a] != '\0'; a++)
+
+	if (s1)
 	{
-		s[a] = s1[a];
+		while (i < len1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
 	}
-	for (b = 0; s2[b] != '\0'; b++)
+	if (s2)
 	{
-		s[a] = s2[b];
-		a++;
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
 	}
-	return (s);
+	s3[i] = '\0';
+
+	return (s3);
 }
