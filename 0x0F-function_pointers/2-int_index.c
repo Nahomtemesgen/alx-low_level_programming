2a132b17e502db334d06bf155d7550dc093e4272
Nahomtemesgen <101422942+Nahomtemesgen@users.noreply.github.com>
2022-05-28 19:29:27 +0300

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Search for the posistion of
+ * an element in array that meet the condition in the cmp function.
+ * @array: Array address.
+ * @size: Array size.
+  * Return: integer.
+ **/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && cmp && size > 0)
+	{
+		for (; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+		return (-1);
+	}
+	return (-1);
+}
