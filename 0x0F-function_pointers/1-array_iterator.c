2fe6735fc5d6d4ede44db2d48dd5fe462b261115
Nahomtemesgen <101422942+Nahomtemesgen@users.noreply.github.com>
2022-05-28 19:28:13 +0300

    1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * array_iterator - Call for another function for each element of an array.
+ * @array: Array address.
+ * @size: Array size.
+ * @action: Function address.
+ **/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (array && action)
+	{
+		for (; i < size; i++)
+			action(array[i]);
+	}
+}
